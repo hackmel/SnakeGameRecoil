4e98acff1bda92d11a959e4d373df78b069a51b6
Rommel Suarez <hackmel@yahoo.com>
2020-09-15 21:27:59 +1200

    Updates

---
+++ precache-manifest.9e5dce1ea5a51bcc2b3d28d6a350c2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8c695b9151959b2b53b4b43a2ff01d",
+    "url": "/SnakeGameRecoil/index.html"
+  },
+  {
+    "revision": "1302e73a118a25907a8e",
+    "url": "/SnakeGameRecoil/static/css/main.ad82459e.chunk.css"
+  },
+  {
+    "revision": "a2de0b0f39fff5610493",
+    "url": "/SnakeGameRecoil/static/js/2.cd08daaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnakeGameRecoil/static/js/2.cd08daaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1302e73a118a25907a8e",
+    "url": "/SnakeGameRecoil/static/js/main.7dfc2b62.chunk.js"
+  },
+  {
+    "revision": "882e6d868be5dbe35b11",
+    "url": "/SnakeGameRecoil/static/js/runtime-main.666630a7.js"
+  }
+]);
